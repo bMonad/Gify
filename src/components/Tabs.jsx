f1ef4afd2d9f4f4777796cade9a5935decb4559a
Shiva1706 <shivansh.singh20021@gmail.com>
2026-01-08 11:33:50 +0530

    Dev Branch initialization

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,13 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveTab } from "../redux/features/searchSlice";
 
 const Tabs = () => {
-  const tabs = ['photos', 'GIFs', 'videos']
+  const tabs = ['photos', 'videos', 'GIFs']
+
+  const dispatch = useDispatch();
+  const activeTab = useSelector((state) => state.search.activeTab);
+
   return (
     <div className="flex gap-10 mt-6 pb-1 justify-evenly border-b border-stone-700">
       {tabs.map((tab, idx) => {
-        return <button key={idx}
-          className="font-medium text-stone-400 uppercase cursor-pointer hover:text-stone-300 hover:scale-103 ease-in-out">
-          {tab}
-        </button>
+        return (
+          <button
+            key={idx}
+            className={`font-medium uppercase cursor-pointer hover:text-[#f9b767] hover:scale-103 ease-in-out ${(activeTab === tab) ? 'text-[#f9b767] scale-103' : 'text-stone-400'} transition-all`}
+            onClick={() => {
+              dispatch(setActiveTab(tab))
+            }}>
+            {tab}
+          </button>
+        )
       })}
     </div>
   )
